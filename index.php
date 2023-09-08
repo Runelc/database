41d85d51c00f506b84c766cb97134c560cf25920
Rune Christoffersen <rune1833@live.dk>
2023-09-08 12:43:42 +0200

    New windows MSN chat project

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
 
+<?php
+echo "<title>Chatbot</title>"; 
+echo "<link rel='stylesheet' href='styles.css'>";
+?>
+
+<body>
+  <div class="MSN-container">
+    <div class="topBar">
+      <div class="topButtonContainer">
+        <button class="miniBut">
+          <div class="miniIcon"></div>
+        </button>
+        <button class="openBut">
+          <div class="openIcon">
+            <div class="openIconSquare"></div>
+          </div>
+        </button>
+        <button class="closeBut">
+          <div class="line1"></div>
+          <div class="line2"></div>
+        </button>
+      </div>
+    </div>
+  <div class="settingsTools">
+    <ul>
+      <li class="fileList"><a href="#">F̲ile</a></li>
+      <li><a href="#">E̲dit</a></li>
+      <li><a href="#">A̲ctions</a></li>
+      <li><a href="#">T̲ools</a></li>
+      <li><a href="#">H̲elp</a></li>
+      </ul>
+  </div>
+  <div class="close-tools"></div>
+  <p class=""></p>
+   </div>
+</body>
