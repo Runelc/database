160c39b9f21ac5d169a897082d19aac1fcb655f6
Rune Christoffersen <rune1833@live.dk>
2023-09-10 12:04:31 +0200

    Prettier for php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-
 <?php
-echo "<title>Chatbot</title>"; 
+echo "<title>Chatbot</title>";
 echo "<link rel='stylesheet' href='styles.css'>";
 ?>
 
@@ -22,77 +21,74 @@ echo "<link rel='stylesheet' href='styles.css'>";
         </button>
       </div>
     </div>
-  <div class="settingsTools">
-    <ul>
-      <li class="fileList"><a href="#">F̲ile</a></li>
-      <li><a href="#">E̲dit</a></li>
-      <li><a href="#">A̲ctions</a></li>
-      <li><a href="#">T̲ools</a></li>
-      <li><a href="#">H̲elp</a></li>
+    <div class="settingsTools">
+      <ul>
+        <li class="fileList"><a href="#">F̲ile</a></li>
+        <li><a href="#">E̲dit</a></li>
+        <li><a href="#">A̲ctions</a></li>
+        <li><a href="#">T̲ools</a></li>
+        <li><a href="#">H̲elp</a></li>
       </ul>
-  </div>
-  <div class="msnTools">
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">🗣️</p>
     </div>
-    <p class="text">I̲nvite</p>
-  </div>
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">📁</p>
-    </div>
-    <p class="text">Send fil̲es</p>
-  </div>
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">📹</p>
-    </div>
-    <p class="text">Video̲</p>
-  </div>
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">🎤</p>
-    </div>
-    <p class="text">Voic̲e</p>
-  </div>
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">🎵</p>
-    </div>
-    <p class="text">Activ̲ities</p>
-  </div>
-  <div class="emojiDiv">
-    <div class="emojiWrapper">
-      <p class="emoji">🎲</p>
-    </div>
-    <p class="text">G̲ames</p>
-  </div>
-  
-</div>
-<div class="messengerContainer">
-  <div class="wholeChat">
-  <div class="pcChatContainer">
-    <div class="pcChatWrapper">
-        <div class="pcChatTop">
-          <p class="pcChatTopText">To:</p>
+    <div class="msnTools">
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">🗣️</p>
         </div>
-        <div class="pcChat"></div>
+        <p class="text">I̲nvite</p>
+      </div>
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">📁</p>
+        </div>
+        <p class="text">Send fil̲es</p>
+      </div>
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">📹</p>
+        </div>
+        <p class="text">Video̲</p>
+      </div>
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">🎤</p>
+        </div>
+        <p class="text">Voic̲e</p>
+      </div>
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">🎵</p>
+        </div>
+        <p class="text">Activ̲ities</p>
+      </div>
+      <div class="emojiDiv">
+        <div class="emojiWrapper">
+          <p class="emoji">🎲</p>
+        </div>
+        <p class="text">G̲ames</p>
+      </div>
+
     </div>
-    <div class="imageWrapper">
-      <div class="outerBox">
-    <img class="pcChatImg" src="./assets/profile.jpeg" alt="profile image">
-    <img class="arrow" src="./assets/arrow.png" alt="arrow">
-    </div>
-    <div class="arrowSide">
-    <img class="arrowSideImage" src="./assets/arrow.png" alt="arrow">
-    </div>
+    <div class="messengerContainer">
+      <div class="wholeChat">
+        <div class="pcChatContainer">
+          <div class="pcChatWrapper">
+            <div class="pcChatTop">
+              <p class="pcChatTopText">To:</p>
+            </div>
+            <div class="pcChat"></div>
+          </div>
+          <div class="imageWrapper">
+            <div class="outerBox">
+              <img class="pcChatImg" src="./assets/profile.jpeg" alt="profile image">
+              <img class="arrow" src="./assets/arrow.png" alt="arrow">
+            </div>
+            <div class="arrowSide">
+              <img class="arrowSideImage" src="./assets/arrow.png" alt="arrow">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  </div>
-</div>
-
-
-
-   </div>
 </body>
